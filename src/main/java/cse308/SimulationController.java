54a9788551d86436297226d529956fd9332a3795
Jaskirat Singh <jaskirat.singh.1@stonybrook.edu>
2018-12-02 10:53:31 -0500

    Added Start Simulation Controller

--- src/main/java/cse308/SimulationController.java
+++ src/main/java/cse308/SimulationController.java
@@ -1,21 +1,34 @@
 package cse308;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import Simulation.FunctionWeights;
+import Simulation.SimulationManager;
+import Simulation.SimulationParams;
 import Users.UserAccount;
 
 @RestController
 public class SimulationController {
-	
-	@RequestMapping(value="/api/start" , method = RequestMethod.POST)
-	public @ResponseBody UserAccount addNewWorker(@RequestBody UserAccount jsonString) {
 
-	    //do business logic
-	    return jsonString;
+	@RequestMapping(value = "/api/start", method = RequestMethod.POST, consumes = "application/json")
+	public void addNewWorker(@RequestBody Map<String, Object> simParams) throws Exception {
+
+		Map<String, Object> b = (Map<String, Object>) simParams.get("functionWeights");
+
+		FunctionWeights functionWeights = new FunctionWeights((float) b.get("wCompactness"),
+				(float) b.get("wPopulationEquality"), (float) b.get("wPartisanFairness"));
+
+		SimulationParams simulationParams = new SimulationParams(functionWeights, (String) simParams.get("state"),
+				(String) simParams.get("algortihm"));
+		
+		UserAccount userAccount = new UserAccount();
+		
+		SimulationManager.createSim(userAccount,  simulationParams);
 	}
 
 }
